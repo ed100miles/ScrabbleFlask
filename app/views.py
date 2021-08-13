e70d43e1890c98beeff771a8e12bf358b997cad4
ed100miles <edmilestech@gmail.com>
2021-08-13 15:31:46 +0100

    basic word scoring implemented

--- app/views.py
+++ app/views.py
@@ -6,10 +6,8 @@ import json
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        json_body = json.loads(request.data.decode('utf-8'))
-        user_letters = json_body['userLetters']
-        board_dict = json_body['board_dict']
-        possible_words = scrabble.find_words(board_dict, user_letters)
+        json_body = json.loads(request.data.decode('utf-8')) # parse json
+        possible_words = scrabble.find_words(json_body['board_dict'], json_body['userLetters'])
         json_response = make_response(jsonify(possible_words), 200)
         return json_response
     return render_template('index.html')
